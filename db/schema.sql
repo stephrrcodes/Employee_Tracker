28c2ed90761f24c89dfd052cb7546afbcc3a1fd4
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-05-15 23:53:13 -0700

    fixed errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,13 @@
 /*  create database  */
-DROP DATABASE IF IT EXISTS employee_db;
+DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
-
-USE DATABASE employee_db;
+USE employee_db;
 
 /*  create department table  */
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     dept_name VARCHAR(30) NOT NULL,
-    );
+);
 
 /*  create role table  */
 CREATE TABLE role (
@@ -16,8 +15,8 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE SET NULL
-    );
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE SET NULL;:
+);
 
 /*  create employee table  */ 
 CREATE TABLE employee (
@@ -26,6 +25,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
-    );
+);
